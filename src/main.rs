7665d723b92ec7cb241f1774d1724c2292a29b6d
Brett Smith <brettsmith212@gmail.com>
2025-09-22 21:24:30 -0700

    alphnumeric

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,8 @@ use std::process;
 fn match_pattern(input_line: &str, pattern: &str) -> bool {
     if pattern == "\\d" {
         input_line.chars().any(|c| c.is_ascii_digit())
+    } else if pattern == "\\w" {
+        input_line.chars().any(|c| c.is_alphanumeric() || c == '_')
     } else if pattern.chars().count() == 1 {
         input_line.contains(pattern)
     } else {
@@ -14,9 +16,6 @@ fn match_pattern(input_line: &str, pattern: &str) -> bool {
 
 // Usage: echo <input_text> | your_program.sh -E <pattern>
 fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    eprintln!("Logs from your program will appear here!");
-
     if env::args().nth(1).unwrap() != "-E" {
         println!("Expected first argument to be '-E'");
         process::exit(1);
